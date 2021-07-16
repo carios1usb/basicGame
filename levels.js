f044f83bc37df1870a4735d4d392b67806031820
Carlos Andres Rios Aristizabal <carlosrios@MacBook-Pro-de-Carlos.local>
2021-07-15 22:39:23 -0500

    Basic music game

---
+++ levels.js
@@ -0,0 +1,32 @@
+var levels = {
+    level1 : {
+        song: new sound("lvl1.mp3"),
+        video: "./level1.mp4",
+        name: "Rock Party",
+        speed: 40,
+        frameToStart: 500, // frames
+        timeToFinish: 70, //Seconds
+        pointsToFinish: 500,
+        obstaclesCurrency: 50
+   },
+   level2 : {
+        song: new sound("lvl2.mp3"),
+        video: "./level2.mp4",
+        name: "Pop Party",
+        speed: 20,
+        frameToStart: 60, // frames
+        timeToFinish: 60, //Seconds
+        pointsToFinish: 550,
+        obstaclesCurrency: 45
+    },  
+    level3 : {
+        song: new sound("lvl3.mp3"),
+        video: "./level3.mp4",
+        name: "Reggae Party",
+        speed: 20,
+        frameToStart: 500, // frames
+        timeToFinish: 80, //Seconds
+        pointsToFinish: 600,
+        obstaclesCurrency: 35
+    }
+}
